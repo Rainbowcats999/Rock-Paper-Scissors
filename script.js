8d11de6fe08e08e30ce033e383d1d07fc3307c74
Rainbowcats999 <526erizov@frhsd.com>
2025-01-08 14:14:53 +0000

    Added in the js rules

---
+++ script.js
@@ -0,0 +1,8 @@
+//Javascript rules for custom cursor//
+const cursor = document.querySelector('.custom-cursor');
+
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = e.clientX + 'px';
+  cursor.style.top = e.clientY + 'px';
+});
+//ends here//
